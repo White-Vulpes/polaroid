4f5a41120db095ff2af0df50aa87fb277b913260
White-Vulpes <aayush.02.parmar@gmail.com>
2024-06-24 04:41:53 +0530

    Campfire Save 3

--- src/pages/galleria/Galleria.js
+++ src/pages/galleria/Galleria.js
@@ -1,34 +1,41 @@
 import './Galleria.css';
+import Welcome from '../../components/Welcome'
+import React from 'react';
 
 function Galleria() {
+
+    React.useEffect(() => {
+        console.log("Hello")
+    }, []);
+
     return(
         <div className='galleria-main'>
-            {/* <div className='galleria-header'>
-                GALLERIA
-            </div> */}
-            <div class="container">
-                <div class="card">
+            <div className='galleria-welcome'>
+                <Welcome/>
+            </div>
+            <div className="container">
+                <div className="card image-1">
                     <div className='heading'>Nature</div>
                 </div>
-                <div class="card">
+                <div className="card image-2">
                     <div className='heading'>Baby</div>
                 </div>
-                <div class="card">
+                <div className="card image-3">
                     <div className='heading'>Model</div>
                 </div>
-                <div class="card">
+                <div className="card image-4">
                     <div className='heading'>Product</div>
                 </div>
-                <div class="card">
+                <div className="card image-5">
                     <div className='heading'>Jewellery</div>
                 </div>
-                <div class="card">
+                <div className="card image-6">
                     <div className='heading'>Fashion</div>
                 </div>
-                <div class="card">
+                <div className="card image-7">
                     <div className='heading'>Maternity</div>
                 </div>
-                <div class="card">
+                <div className="card image-8">
                     <div className='heading'>Monument</div>
                 </div>
             </div>
