14447db2579ed1d73f085877c82b175d138eb713
White-Vulpes <aayush.02.parmar@gmail.com>
2024-09-11 22:41:47 +0530

    ahh bugs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import './App.css';
-import Home from './pages/home/Home'
-import Galleria from './pages/galleria/Galleria';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import Home from "./pages/home/Home";
+import Galleria from "./pages/galleria/Galleria";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <HashRouter>
         <Routes>
-          <Route path="/" element={ <Home /> } />
-          <Route path="/galleria" element={ <Galleria /> } />
+          <Route path="/" element={<Home />} />
+          <Route path="/galleria" element={<Galleria />} />
         </Routes>
       </HashRouter>
     </div>
